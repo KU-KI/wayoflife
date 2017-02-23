14a8048997684b7400c3c5b6dc95dd907bc49616
Hucoo <majkos72@gmail.com>
2017-02-23 11:13:19 +0100

    Create database.php

---
+++ core/database.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "us-cdbr-azure-southcentral-f.cloudapp.net";
+$username = "b1f5a0707ed7d6";
+$password = "89ac8c94";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
