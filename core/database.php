5bb2bb7aa82d6554e586aae7c48b1b99382526bf
Hucoo <majkos72@gmail.com>
2017-02-23 11:14:38 +0100

    Update database.php

--- core/database.php
+++ core/database.php
@@ -1,4 +1,6 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 $servername = "us-cdbr-azure-southcentral-f.cloudapp.net";
 $username = "b1f5a0707ed7d6";
 $password = "89ac8c94";
