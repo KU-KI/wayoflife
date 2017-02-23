c22a498db510023ae3b86cb81f866fd963b3c156
Hucoo <majkos72@gmail.com>
2017-02-23 10:18:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -218,7 +218,7 @@ h5,
   
 <div class="bg indigo">
   <h1>Welcome at</h1>
-  <div class="domain white indigo-text z-depth-1">insert-url-here.com</div>
+  <div class="domain white indigo-text z-depth-1">www.wayoflife.azurewebsites.net</div>
 </div>
 <div class="container white z-depth-1 anim-0">
   <div class="row">
