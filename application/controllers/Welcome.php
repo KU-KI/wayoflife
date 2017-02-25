4907ea6ad431c437a9d0b660bf1fa24c6d8dd2f6
majko <majko@DESKTOP-PHMSSE7>
2017-02-25 16:11:20 +0100

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -15,7 +15,6 @@ class Welcome extends CI_Controller {
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
