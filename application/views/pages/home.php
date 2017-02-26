3069b48c9f862a489faeb01f22ccd5993e0e7e66
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 11:06:01 +0100

    update

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -11,20 +11,7 @@
             <input type="text" placeholder="Používateľské meno" />
             <input type="password" placeholder="Heslo" />
             <button>Prihlásiť sa </button>
-            <?php if (@$user_profile): ?>
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <img class="img-thumbnail" data-src="holder.js/140x140" alt="140x140" src="https://graph.facebook.com/<?=$user_profile['id']?>/picture?type=large" style="width: 140px; height: 140px;" />
-                    <h2>
-                        <?=$user_profile['name']?>
-                    </h2>
-                    <a href="<?= $logout_url ?>" class="btn btn-lg btn-primary btn-block" role="button">Logout</a>
-                </div>
-            </div>
-            <?php else: ?>
-            <h2 class="form-signin-heading">Login with Facebook</h2>
-            <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block" role="button">Login</a>
-            <?php endif; ?>
+            <button>Alebo skús FACEBOOK</button>
         </form>
     </div>
     <div class="form">
@@ -40,4 +27,20 @@
     <div class="cta">
         <a rel="nofollow" rel="noreferrer" href="#">Zabudol si heslo?</a>
     </div>
+    <form class="form-signin" role="form">
+        <?php if (@$user_profile): ?>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <img class="img-thumbnail" data-src="holder.js/140x140" alt="140x140" src="https://graph.facebook.com/<?=$user_profile['id']?>/picture?type=large" style="width: 140px; height: 140px;" />
+                <h2>
+                    <?=$user_profile['name']?>
+                </h2>
+                <a href="<?= $logout_url ?>" class="btn btn-lg btn-primary btn-block" role="button">Logout</a>
+            </div>
+        </div>
+        <?php else: ?>
+        <h2 class="form-signin-heading">Login with Facebook</h2>
+        <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block" role="button">Login</a>
+        <?php endif; ?>
+    </form>
 </div>
