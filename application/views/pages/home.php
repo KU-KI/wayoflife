ee6c0b41d5957dae415ba12558f18ac40c9d6b5d
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 14:23:46 +0100

    update

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -4,47 +4,53 @@ if (@$user_profile):
 
     redirect('home/login');
 
-else: 
+else:
 
 ?>
-<div class="container text-center"><h1>Way of life</h1></div>
+<div class="container text-center">
+    <h1>Way of life</h1>
+</div>
 <div class="container">
-    <div class="module form-module">
-        <div class="toggle">
-            <i class="fa fa-times fa-pencil"></i>
-            <div class="tooltip">Click Me</div>
-        </div>
-        <div class="form">
-            <h2>Prihlásiť sa do účtu</h2>
-            <form>
-                <input type="text" placeholder="Používateľské meno" />
-                <input type="password" placeholder="Heslo" />
-                <button>prihlásiť sa </button>
-                    <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block facebook" role="button">... facebook</a>
-            </form>
-        </div>
+    <div class="row">
+        <div class="col-md-5 col-md-offset-5">
+            <div class="module form-module">
+                <div class="toggle">
+                    <i class="fa fa-times fa-pencil"></i>
+                    <div class="tooltip">Click Me</div>
+                </div>
+                <div class="form">
+                    <h2>Prihlásiť sa do účtu</h2>
+                    <form>
+                        <input type="text" placeholder="Používateľské meno" />
+                        <input type="password" placeholder="Heslo" />
+                        <button>prihlásiť sa </button>
+                        <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block facebook" role="button">... facebook</a>
+                    </form>
+                </div>
 
-        <div class="form">
-            <h2>Vytvorenie účtu</h2>
-            <form>
-                <input type="text" placeholder="Používateľské meno" />
-                <input type="password" placeholder="Heslo" />
-                <input type="email" placeholder="E-mail" />
-                <button>registrovať</button>
+                <div class="form">
+                    <h2>Vytvorenie účtu</h2>
+                    <form>
+                        <input type="text" placeholder="Používateľské meno" />
+                        <input type="password" placeholder="Heslo" />
+                        <input type="email" placeholder="E-mail" />
+                        <button>registrovať</button>
 
-                <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block facebook" role="button">... facebook</a>
+                        <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block facebook" role="button">... facebook</a>
 
 
-            </form>
-        </div>
+                    </form>
+                </div>
 
-        <div class="cta">
-            <a rel="nofollow" rel="noreferrer" href="#">Zabudol si heslo?</a>
+                <div class="cta">
+                    <a rel="nofollow" rel="noreferrer" href="#">Zabudol si heslo?</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-<?php 
+<?php
 
-endif; 
+endif;
 
 ?>
