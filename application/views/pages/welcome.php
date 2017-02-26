599b4075dfd359374f2d01dfb07c2b10ca25372b
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 16:27:09 +0100

    update

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,5 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<?php if (@$user_profile or $this->session->userdata('user_name')!= NULL): ?>
+<?php if (@$user_profile or $this->session->userdata('user_name')!= NULL):
+if ($user_profile['name'] == NULL){
+    $name=$this->session->userdata('user_name');
+}esle{
+    $name=$user_profile['name'];
+ }
+
+
+?>
 <div class="container text-center">
     <h1>Way of life</h1>
 </div>
@@ -7,7 +15,7 @@
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <div class="module form-module thanks">
-                <h2>Túto časť môže vidieť jedine prihlásený! <?php echo $user_profile['name']; ?> <?php echo $this->session->userdata('user_name'); ?><?php echo anchor('home/logout', 'Odhlásiť'); ?></h2>
+                <h2>Túto časť môže vidieť jedine prihlásený! <?php echo $name; ?><?php echo anchor('home/logout', 'Odhlásiť'); ?></h2>
             </div>
         </div>
     </div>
