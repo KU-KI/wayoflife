04dc2e4a617415aad9bbcabca69da6f03b1691c7
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 15:14:36 +0100

    registration

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -85,6 +85,25 @@ class Home extends CI_Controller {
         $this->load->view('templates/footer', $data);
 
     }
+    public function registration()
+    {
+        $this->load->library('form_validation');
+        // field name, error message, validation rules
+        $this->form_validation->set_rules('user_name', 'User Name', 'trim|required|min_length[4]|xss_clean');
+        $this->form_validation->set_rules('email_address', 'Your Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[32]');
+        $this->form_validation->set_rules('con_password', 'Password Confirmation', 'trim|required|matches[password]');
+
+        if($this->form_validation->run() == FALSE)
+        {
+            $this->index();
+        }
+        else
+        {
+            $this->user_model->add_user();
+            $this->thank();
+        }
+    }
 
 }
 
