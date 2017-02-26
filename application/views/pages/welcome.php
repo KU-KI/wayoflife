e7ec0fcce61a465edfb28c600c22abb5d31b826b
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 16:29:01 +0100

    update

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,10 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <?php if (@$user_profile or $this->session->userdata('user_name')!= NULL):
-if ($user_profile['name'] == NULL){
+if ($user_profile['name'] == NULL):
     $name=$this->session->userdata('user_name');
-}esle{
+else:
     $name=$user_profile['name'];
- }
+endif;
 
 
 ?>
