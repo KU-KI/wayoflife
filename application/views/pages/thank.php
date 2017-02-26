07d85106857e5b4699ffee74e85e7f45f25b9fc1
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 15:26:40 +0100

    thankyou

---
+++ application/views/pages/thank.php
@@ -0,0 +1,26 @@
+﻿<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+      if (@$user_profile):
+
+          redirect('home/login');
+
+      else:
+
+?>
+<div class="container text-center">
+    <h1>Way of life</h1>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="module form-module">
+                <h2>Ďakujeme za registráciu!</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+
+      endif;
+
+?>
