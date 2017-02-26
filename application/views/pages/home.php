cf8e6ef7eed66271a854300d59830287c2eff7f2
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 10:32:35 +0100

    login page

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,2 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
-<h1>Hello, world!</h1>
+<!-- Form Module-->
+<div class="module form-module">
+    <div class="toggle">
+        <i class="fa fa-times fa-pencil"></i>
+        <div class="tooltip">Click Me</div>
+    </div>
+    <div class="form">
+        <h2>Login to your account</h2>
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+        </form>
+    </div>
+    <div class="form">
+        <h2>Create an account</h2>
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <input type="email" placeholder="Email Address" />
+            <input type="tel" placeholder="Phone Number" />
+            <button>Register</button>
+        </form>
+    </div>
+    <div class="cta">
+        <a rel="nofollow" rel="noreferrer" href="#">Forgot your password?</a>
+    </div>
+</div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,5 +3,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url("application/views/assets/js/bootstrap.min.js"); ?>"></script>
+    <script>
+        // Toggle Function
+    $('.toggle').click(function(){
+      // Switches the Icon
+      $(this).children('i').toggleClass('fa-pencil');
+      // Switches the forms
+      $('.form').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: "toggle"
+      }, "slow");
+    });
+    </script>
   </body>
 </html>
