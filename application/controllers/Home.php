8d1a703422b5c732d11e7c369c78db25a15e9073
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 11:09:06 +0100

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,43 +7,13 @@ class Home extends CI_Controller {
         $this->load->helper('url');
 	}
 
-	public function login(){
-
-		$this->load->library('facebook');
-
-		$user = $this->facebook->getUser();
-
-        if ($user) {
-            try {
-                $data['user_profile'] = $this->facebook->api('/me');
-            } catch (FacebookApiException $e) {
-                $user = null;
-            }
-        }else {
-
-        }
-
-        if ($user) {
-
-            $data['logout_url'] = site_url('home/logout');
-
-        } else {
-            $data['login_url'] = $this->facebook->getLoginUrl(array(
-                'redirect_uri' => site_url('home/login'),
-                'scope' => array("email")
-            ));
-        }
-        $this->load->view('login',$data);
-
-	}
-
     public function logout(){
 
         $this->load->library('facebook');
 
         $this->facebook->destroySession();
 
-        redirect('home/login');
+        redirect('');
     }
 
     public function index($page = 'home')
@@ -69,7 +39,7 @@ class Home extends CI_Controller {
 
         } else {
             $data['login_url'] = $this->facebook->getLoginUrl(array(
-                'redirect_uri' => site_url('home/login'),
+                'redirect_uri' => site_url(''),
                 'scope' => array("email")
             ));
         }
