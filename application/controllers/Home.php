bb8dcd91980ce0b15c918b9e1f036fd3d84dd872
majko <majko@DESKTOP-PHMSSE7>
2017-02-26 09:42:10 +0100

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -48,11 +48,6 @@ class Home extends CI_Controller {
 
     public function index($page = 'index_page')
 	{
-        if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
-        {
-            show_404();
-        }
-
         $data['title'] = ucfirst($page);
 
         $this->load->view('templates/header', $data);
