99b5ff68e9f5f3d1b1d77d6d928e6632b67f3d85
MartinPlevjak <m.plevjak@gmail.com>
2017-02-27 17:21:10 +0100

    up

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -57,7 +57,7 @@
                     <li class="active">
                         <a href="dashboard.html">
                             <i class="ti-panel"></i>
-                            <p>Dashboard</p>
+                            <?php echo $user_profile['name']; ?><?php echo $this->session->userdata('user_name'); ?><?php echo anchor('home/logout', 'Odhlásiť'); ?>
                         </a>
                     </li>
                     <li>
@@ -422,31 +422,6 @@
 
 </html>
 
-
-
-
-
-
-
-
-
-
-
-
-
-<div class="container text-center">
-    <h1>Way of life</h1>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-            <div class="module form-module thanks">
-                <h2>Túto časť môže vidieť jedine prihlásený! <?php echo $user_profile['name']; ?> <?php echo $this->session->userdata('user_name'); ?><?php echo anchor('home/logout', 'Odhlásiť'); ?></h2>
-            </div>
-        </div>
-    </div>
-
-</div>
 <?php
 else:
 
