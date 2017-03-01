42d819c10a5fad02fb5965a252a11560e656fb03
Hucoo <majkos72@gmail.com>
2017-03-01 13:35:23 +0100

    update

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -31,7 +31,7 @@ else:
                     <?php echo form_open("home/login"); ?>
                         <input type="text" id="email" name="email" value="" placeholder="E-mail" />
                         <input type="password" id="pass" name="pass" value="" placeholder="Heslo" />
-                        <input type="submit" class="" value="Sign in" />
+                        <input type="submit" class="" value="Prihlásiť" />
                         <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block facebook" role="button">... facebook</a>
                     <?php echo form_close(); ?>
                 </div>
@@ -43,7 +43,7 @@ else:
                         <input type="text" id="user_name" name="user_name" value="<?php echo set_value('user_name'); ?>" placeholder="Používateľské meno" />
                         <input type="password" id="password" name="password" value="<?php echo set_value('password'); ?>" placeholder="Heslo" />
                         <input type="text" id="email_address" name="email_address" value="<?php echo set_value('email_address'); ?>" placeholder="E-mail" />
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Registrovať" />
 
                         <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block facebook" role="button">... facebook</a>
 
