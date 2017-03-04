7fc9856944473b8e0ae89f290fe4396522fcfcae
Hucoo <majko@DESKTOP-PHMSSE7>
2017-03-04 19:47:21 +0100

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# wayoflife
-School project
--- application/config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-
-$config['appId']   = '1827407794185012';
-$config['secret']  = 'dbc0b5b9c1686f3da9dd060f882cc7f8';
-
-?>
