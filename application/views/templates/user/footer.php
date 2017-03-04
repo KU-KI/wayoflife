8985a8d4b7f2ab31fab7ea301e60a5c4ff20f5fb
Hucoo <majkos72@gmail.com>
2017-03-04 14:17:11 +0100

    update

--- application/views/templates/user/footer.php
+++ application/views/templates/user/footer.php
@@ -1,13 +1,13 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="copyright pull-right">
-            &copy;
-            <script>document.write(new Date().getFullYear())</script>, made with
-            <i class="fa fa-heart heart"></i>by
-            <a href="#">Trpaslici</a>
-        </div>
-    </div>
-</footer>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="copyright pull-right">
+                        &copy;
+                        <script>document.write(new Date().getFullYear())</script>, made with
+                        <i class="fa fa-heart heart"></i>by
+                        <a href="#">Trpaslici</a>
+                    </div>
+                </div>
+            </footer>
 
         </div>
     </div>
