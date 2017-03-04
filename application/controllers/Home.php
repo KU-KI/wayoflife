e8f340d8981312e0c16839b4055da542bc63665a
Hucoo <majkos72@gmail.com>
2017-03-04 14:08:26 +0100

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -67,7 +67,9 @@ class Home extends CI_Controller {
 
         $data['title'] = ucfirst($page);
 
+        $this->load->view('templates/user/header', $data);
         $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/user/footer', $data);
 
     }
     public function login()
