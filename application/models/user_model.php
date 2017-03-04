f5b168a434efabb09611eceb126dfaf37972e437
Hucoo <majkos72@gmail.com>
2017-03-04 11:01:42 +0100

    update

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -25,7 +25,7 @@
                   $this->session->set_userdata($newdata);
                   return true;
               }else{
-                  echo 'Niekde nastala chyba, skús to znova';
+                  echo '<div style="position:absolute; top:0; width:100%; line-height:50px; background:rgba(192, 57, 43,1.0);" >Nikde nastala chyba, skús to znova</div>';
                   return false;
               }
           }
