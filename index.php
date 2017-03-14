8883f09aecda55e16a3a6628c1bd37c6e874ce3d
Hucoo <majko@DESKTOP-PHMSSE7>
2017-03-14 17:28:51 +0100

    update

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ session_start();
                     </div>
                     <div class="info-text-in">Kontroluj štatistiky, koľko si pribral, koľko si schudol a ako sa za celé obdobie zmenila tvoja váha! To všetko na jednom mieste!</div>
                 </div>
-                <div class="col-md-3 text-center">
+                <div class="col-md-3 text-center login-side">
                     <h2>Way of life</h2>
                     <button type="button" class="btn btn-default btn-login" data-toggle="modal" data-target="#myModal">
                         Prihlásiť sa
