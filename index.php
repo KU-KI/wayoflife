d0c83ad74b99f058f5c7e4ae8b6957fc5cc80417
Hucoo <majko@DESKTOP-PHMSSE7>
2017-03-14 17:02:24 +0100

    update

--- index.php
+++ index.php
@@ -35,40 +35,13 @@ session_start();
         <div class="col-md-12 nopadding">
             <div class="owl-carousel owl-theme">
                 <div class="item">
-                    <h4>1</h4>
+                    <img src="assets/img/slider.jpg" />
                 </div>
                 <div class="item">
-                    <h4>2</h4>
+                    <img src="assets/img/slider.jpg" />
                 </div>
                 <div class="item">
-                    <h4>3</h4>
-                </div>
-                <div class="item">
-                    <h4>4</h4>
-                </div>
-                <div class="item">
-                    <h4>5</h4>
-                </div>
-                <div class="item">
-                    <h4>6</h4>
-                </div>
-                <div class="item">
-                    <h4>7</h4>
-                </div>
-                <div class="item">
-                    <h4>8</h4>
-                </div>
-                <div class="item">
-                    <h4>9</h4>
-                </div>
-                <div class="item">
-                    <h4>10</h4>
-                </div>
-                <div class="item">
-                    <h4>11</h4>
-                </div>
-                <div class="item">
-                    <h4>12</h4>
+                    <img src="assets/img/slider.jpg" />
                 </div>
             </div>
         </div>
