b71b410b5d19228fe2017be93c955bfb82094e6c
Hucoo <majko@DESKTOP-PHMSSE7>
2017-03-14 17:12:57 +0100

    update

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ session_start();
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet" />
 
     <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
     <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
