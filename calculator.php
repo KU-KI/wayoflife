0fd0e82a43b5405ef94af3a4a9787bcc85cf1f8a
Hucoo <majko@DESKTOP-PHMSSE7>
2017-03-15 13:10:02 +0100

    update

--- calculator.php
+++ calculator.php
@@ -1,9 +1,24 @@
-<?php include_once 'template/header.php'; ?>
+﻿<?php include_once 'template/header.php'; ?>
 
 <div class="content">
     <div class="container-fluid">
         <div class="row">
+            <div class="col-md-4">
+                <form>
+                    <div class="form-group">
+                        <label for="vyska">Výška</label>
+                        <input type="number" class="form-control" id="vyska" placeholder="Výška" />
+                    </div>
+                    <div class="form-group">
+                        <label for="vaha">Váha</label>
+                        <input type="number" class="form-control" id="vaha" placeholder="Váha" />
+                    </div>
+                    <button type="submit" class="btn btn-default">Vypočítať</button>
+                </form>
+            </div>
+            <div class="col-md-8">
 
+            </div>
         </div>
     </div>
 </div>
