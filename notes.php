2c38a57a940da9b8d0ff9760018f82d4bb1a8062
MartinPlevjak <m.plevjak@gmail.com>
2017-05-03 13:25:22 +0200

    Update notes.php

--- notes.php
+++ notes.php
@@ -15,6 +15,28 @@
                             </small>
                         </blockquote>
                     </div>
+                    <div class="typo-line">
+                        <p class="category">Quote</p>
+                        <blockquote>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                            </p>
+                            <small>
+                                Steve Jobs, CEO Apple
+                            </small>
+                        </blockquote>
+                    </div>
+                    <div class="typo-line">
+                        <p class="category">Quote</p>
+                        <blockquote>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                            </p>
+                            <small>
+                                Steve Jobs, CEO Apple
+                            </small>
+                        </blockquote>
+                    </div>
                 </div>
             </div>
         </div>
